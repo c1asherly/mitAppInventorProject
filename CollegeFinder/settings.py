44f2cef34e3a8603af17ceacce1550c3ac7bc03f
Alan Le <alann_le@icloud.com>
2019-11-06 19:37:27 -0800

    Turn off debugging.

--- CollegeFinder/settings.py
+++ CollegeFinder/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!(c_3e3le5lzz$1-e4_^qggw8=niv2)%f^d=6euo@%vylob^e#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://mitappinventorproject.herokuapp.com/']
 
