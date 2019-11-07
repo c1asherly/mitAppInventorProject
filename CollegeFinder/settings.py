1d91d02db7039db93e66a3455ea1d044fc08503f
Alan Le <alann_le@icloud.com>
2019-11-06 17:15:26 -0800

    Adjust Heroku settings

--- CollegeFinder/settings.py
+++ CollegeFinder/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = ( os.path.join('static'), )
+django_heroku.settings(locals())
