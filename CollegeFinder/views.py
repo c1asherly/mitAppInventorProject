a986a853886dd041e82e4875f0c77d58dd55992b
Alan Le <alann_le@icloud.com>
2019-11-06 18:00:25 -0800

    Replace all whitespaces with + so that the external API can recognize the code.

--- CollegeFinder/views.py
+++ CollegeFinder/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render_to_response
 import json
 import urllib.request
+import urllib.parse as urlparse
 api_key = "&api_key=wuH4hRXxyQgUVH9NBEPbd4RARpKyMMIg7wXq9dC5"
 
 fields = """&_fields=id,school.name,latest.admissions.act_scores.midpoint.cumulative,latest.admissions.admission_rate.overall,latest.completion.completion_rate_4yr_150nt,school.city,latest.cost.tuition.in_state,latest.student.size,latest.admissions.sat_scores.midpoint.math,latest.admissions.sat_scores.midpoint.critical_reading,latest.admissions.admission_rate.overall,latest.admissions.sat_scores.average.overall,latest.student.enrollment.all"""
 
 def home(request):
     if request.GET.get('school.name'): #Check if there are tags
-        print(fields)
         schoolname = request.GET.get('school.name')
         schoolstate = request.GET.get('school.state')
         usermath = request.GET.get('satmath')
         userreading = request.GET.get('satreading')
         youract = request.GET.get('act')
-
+        schoolname = schoolname.replace(" ", "+")
         completedurl = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=" + schoolname + "&school.state=" + schoolstate + fields + api_key
+
         print(completedurl)
         response = urllib.request.urlopen(completedurl)
         parsedjson = json.loads(response.read().decode())
